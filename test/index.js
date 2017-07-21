af60397406b83b520d70a8a38e3f5fcd8537400f
letrungkien0210 <letrungkien0210@gmail.com>
2017-07-21 21:47:58 +0700

    Create mutations for object (#2)
    * [Create mutations for object] - Implemented the function which delete a entity by id.
    New issue: show the error 500 when run with test server.
    
    * Fixed the issue: show the error 500 when run with test server.
    
    * [Create mutations for object] - Implemented the function which create a entity.
    
    * [Create mutations for object] - Implemented the function which update a entity by id.
    
    * [Create mutations for object] - Update tests for updating entity.
    
    * [Create mutations for object] - Remove the comment codes.
    
    * Change name of GraphQLInputObjectType.
    Don't show id field in params when create a entity.
    
    * Removed the console.log.

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const CoreData = require("js-core-data");
 const supertest = require("supertest");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const lib = require("../index");
 
@@ -10,21 +11,22 @@ const database = new CoreData("sqlite://:memory:");
 database.createModelFromYaml(fs.readFileSync(__dirname + "/schema.yml"));
 
 const app = express();
+app.use(bodyParser.json());
 app.use("/graphql", lib.graphql(database));
 
 const test = supertest(app);
 
 describe("graphql", () => {
   beforeEach(() => {
-    return database.syncSchema({ force: true }).then(() => {
+    return database.syncSchema({force: true}).then(() => {
       const context = database.createContext();
 
-      context.create("Company", { name: "test" });
-      context.create("Company", { name: "test2" });
-      context.create("Company", { name: "test3" });
+      context.create("Company", {name: "test"});
+      context.create("Company", {name: "test2"});
+      context.create("Company", {name: "test3"});
 
-      context.create("Person", { firstname: "john", lastname: "Doe" });
-      context.create("Person", { firstname: "Jane", lastname: "Siri" });
+      context.create("Person", {firstname: "john", lastname: "Doe"});
+      context.create("Person", {firstname: "Jane", lastname: "Siri"});
 
       return context.save();
     });
@@ -60,9 +62,202 @@ describe("graphql", () => {
   it("should get person", () => {
     return test
       .get(`/graphql?query={getPerson(id:1){id}}`)
-      // .expect(200)
+      .expect(200)
       .then(res => {
         assert.equal(res.body.data.getPerson.id, 1);
+      });
+  })
+
+  it("delete a company by id", () => {
+    let postData = {
+      query: `mutation deleteCompany($id: Int){
+                deleteCompany(id: $id){
+                    id
+                    name
+                    employees{
+                      id
+                    }
+                }
+            }`,
+      variables: {
+        id: 1
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.deleteCompany.id, 1);
+      });
+  });
+
+  it("delete a person by id", () => {
+    let postData = {
+      query: `mutation deletePerson($id: Int){
+                deletePerson(id: $id){
+                  id,
+                  firstname,
+                  lastname,
+                  age,
+                  salary,
+                  birthdate
+                }
+            }`,
+      variables: {
+        id: 1
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.deletePerson.id, 1);
+      });
+  });
+
+  it("create a company", () => {
+    let postData = {
+      query: `mutation createCompany($input:  CompanyCreateInputType){
+                createCompany(input: $input){
+                  name
+                }
+            }`,
+      variables: {
+        input: {
+          name: 'Company A'
+        }
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.createCompany.name, "Company A");
+      });
+  })
+
+  it("create a person", () => {
+    let postData = {
+      query: `mutation createPerson($input:  PersonCreateInputType){
+                createPerson(input: $input){
+                  firstname,
+                  lastname,
+                  age,
+                  salary,
+                  birthdate
+                }
+            }`,
+      variables: {
+        input: {
+          firstname: 'FN',
+          lastname: 'LN',
+          age: 20,
+          salary: 20,
+          birthdate: new Date('01/01/2017')
+        }
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.createPerson.firstname, "FN");
+        assert.equal(res.body.data.createPerson.lastname, "LN");
+        assert.equal(res.body.data.createPerson.age, 20);
+        assert.equal(res.body.data.createPerson.salary, 20);
+        assert.equal(res.body.data.createPerson.birthdate, new Date('01/01/2017'));
+      });
+  });
+
+  it("update a company", () => {
+    let postData = {
+      query: `mutation updateCompany($input:  CompanyUpdateInputType){
+                updateCompany(input: $input){
+                  id,
+                  name
+                }
+            }`,
+      variables: {
+        input: {
+          id: 1,
+          name: 'Company A'
+        }
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.updateCompany.id, 1);
+        assert.equal(res.body.data.updateCompany.name, "Company A");
+      }).then(() => {
+        return test
+          .get(`/graphql?query={getCompany(id:1){id, name}}`)
+          .expect(200)
+          .then(res => {
+            assert.equal(res.body.data.getCompany.id, 1);
+            assert.equal(res.body.data.getCompany.name, "Company A");
+          });
+      });
+  })
+
+  it("update a person", () => {
+    let postData = {
+      query: `mutation updatePerson($input:  PersonUpdateInputType){
+                updatePerson(input: $input){
+                  id,
+                  firstname,
+                  lastname,
+                  age,
+                  salary,
+                  birthdate
+                }
+            }`,
+      variables: {
+        input: {
+          id: 1,
+          firstname: 'FN',
+          lastname: 'LN',
+          age: 20,
+          salary: 20,
+          birthdate: new Date('01/02/2017')
+        }
+      }
+    };
+
+    return test
+      .post(`/graphql?`)
+      .send(postData)
+      .expect(200)
+      .then(res => {
+        assert.equal(res.body.data.updatePerson.id, 1);
+        assert.equal(res.body.data.updatePerson.firstname, "FN");
+        assert.equal(res.body.data.updatePerson.lastname, "LN");
+        assert.equal(res.body.data.updatePerson.age, 20);
+        assert.equal(res.body.data.updatePerson.salary, 20);
+        assert.equal(res.body.data.updatePerson.birthdate, new Date('01/02/2017'));
+      }).then(() => {
+        return test
+          .get(`/graphql?query={getPerson(id:1){id, firstname, lastname, age, salary, birthdate}}`)
+          .expect(200)
+          .then(res => {
+            assert.equal(res.body.data.getPerson.id, 1);
+            assert.equal(res.body.data.getPerson.firstname, "FN");
+            assert.equal(res.body.data.getPerson.lastname, "LN");
+            assert.equal(res.body.data.getPerson.age, 20);
+            assert.equal(res.body.data.getPerson.salary, 20);
+            assert.equal(res.body.data.getPerson.birthdate, new Date('01/02/2017'));
+          });
       });
   })
 });
